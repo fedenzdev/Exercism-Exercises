1795e64af5cddf267d2940b28854f76f98defc7d
FedeNZ <fedenz.dev@gmail.com>
2022-06-27 17:26:04 -0300

    DONE!

--- highScoreBoard.js
+++ highScoreBoard.js
@@ -4,8 +4,36 @@ function createScoreBoard() {
 } */
 
 /* Instruction 2
-
 function addPlayer(scoreBoard, player, score) {
  scoreBoard[player] = score;
    return scoreBoard
 } */
+
+/* Instruction 3 
+
+function removePlayer(scoreBoard, player) {
+   delete scoreBoard[player];
+   return scoreBoard
+} */
+
+/* Instruction 4 
+function updateScore(scoreBoard, player, points) {
+  scoreBoard[player] += points;
+  return scoreBoard
+} */
+
+/* Instruction 5 
+function applyMondayBonus(scoreBoard) {
+   for (let key in scoreBoard) {
+      scoreBoard[key] += 100;
+    }
+    return scoreBoard
+ } */
+
+ /* Instruction 6  
+ function normalizeScore(params) {
+  return params.normalizeFunction(params.score);
+ } */
+
+ DONE!
+
