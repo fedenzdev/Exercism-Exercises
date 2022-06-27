657998fbb82770a8538907c7bac3ffa30d630ebf
FedeNZ <fedenz.dev@gmail.com>
2022-06-27 16:50:46 -0300

    instruction 2 OK

---
+++ highScoreBoard.js
@@ -0,0 +1,11 @@
+/* Instruction 1
+function createScoreBoard() {
+ return {'The Best Ever': 1000000}
+} */
+
+/* Instruction 2
+
+function addPlayer(scoreBoard, player, score) {
+ scoreBoard[player] = score;
+   return scoreBoard
+} */
