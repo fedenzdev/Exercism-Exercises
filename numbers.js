2a563f55422480a9980e784b927c99ee2995ba4f
FedeNZ <fedenz.dev@gmail.com>
2022-06-30 10:52:56 -0300

    instruction 1 ok

---
+++ numbers.js
@@ -0,0 +1,12 @@
+function twoSum(array1, array2) {
+    let arr1 = Number(array1.join("")); 
+    let arr2 = Number(array2.join("")); 
+    return arr1 + arr2
+}
+
+function luckyNumber(value) {
+  
+}
+
+luckyNumber(1441); //=>  true
+luckyNumber(123); //=> false
