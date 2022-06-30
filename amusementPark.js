09b69840fd5d6ec8dda9591cc8544102d53e0605
FedeNZ <fedenz.dev@gmail.com>
2022-06-30 06:06:06 -0300

    done 1-2

---
+++ amusementPark.js
@@ -0,0 +1,26 @@
+/*
+function createVisitor(name, age, ticketId) {
+    let obj = {};
+    obj.name = name,
+    obj.age = age,
+    obj.ticketId = ticketId
+    return obj
+}
+
+const visitor = {
+    name: 'Verena Nardi',
+    age: 45,
+    ticketId: 'H32AZ123',
+  };
+  
+function revokeTicket(visitor) {
+    visitor.ticketId = null;
+    return visitor
+}
+*/
+
+function ticketStatus(tickets, ticketId) {
+
+}
+
+
